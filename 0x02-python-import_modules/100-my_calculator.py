826771f2f6f98eaecaac196a1c0a8280c0725321
Kaleidiana <kaleidiana63@gmail.com>
2022-09-23 05:07:50 -0700

    a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-
-
-def main():
-    if (len(argv) != 4):
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    elif (str(argv[2]) not in ["+", "-", "*", "/"]):
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    else:
-        if (argv[2] == "+"):
-            print(f"{argv[1]} + {argv[3]} = {add(int(argv[1]), int(argv[3]))}")
-    elif (argv[2] == "-"):
-        print(f"{argv[1]} - {argv[3]} = {sub(int(argv[1]), int(argv[3]))}")
-    elif (argv[2] == "*"):
-        print(f"{argv[1]} * {argv[3]} = {mul(int(argv[1]), int(argv[3]))}")
-    else:
-        print(f"{argv[1]} / {argv[3]} = {div(int(argv[1]), int(argv[3]))}")
-
 
 if __name__ == "__main__":
-    main()
+    import sys
+
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
