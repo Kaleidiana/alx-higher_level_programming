8a89d5bf0158c9187086f7a0d75cbb03e75a04b3
Kaleidiana <kaleidiana63@gmail.com>
2022-10-12 15:48:59 +0000

    a class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""class Square that defines a square"""
+"""defines a Square Class"""
 
-class Square():
-    """square class with it's size and proper validation"""
+
+class Square:
+    """Represents a Square"""
 
     def __init__(self, size=0):
-        if (type(size) is not int):
-           raise TypeError("size must be an integer")
-        elif (size < 0):
+        """initializes"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+             self.__size = size
